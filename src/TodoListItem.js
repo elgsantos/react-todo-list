60a8273d7abb100149de25a854fbca9a10f23f2e
Eduardo Gomes <elgsantos@gmail.com>
2019-11-20 19:13:43 -0300

    passando parametros entre componentes

---
+++ src/TodoListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+export function TodoListItem(props) {
+	return (
+		<div>
+			<b>{props.name}</b>
+			<button onClick={() => props.onShowTaskName(props.name)}>
+				Alert
+			</button>
+		</div>
+	);
+}
+
+TodoListItem.propTypes = {
+	id: PropTypes.number,
+	name: PropTypes.string,
+	status: PropTypes.string,
+	onShowTaskName: PropTypes.func
+};
